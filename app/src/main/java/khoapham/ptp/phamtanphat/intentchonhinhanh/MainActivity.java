ba6fc46c0021822d49dbab0e946b345c88219cb5
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-14 20:34:34 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentchonhinhanh/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentchonhinhanh/MainActivity.java
@@ -3,7 +3,12 @@ package khoapham.ptp.phamtanphat.intentchonhinhanh;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
         imgHinhgoc = findViewById(R.id.imageviewHinhgoc);
 
         mangtenhinh = getResources().getStringArray(R.array.arrayanimal);
+//        Random random = new Random();
+//        int index = random.nextInt(mangtenhinh.length);
+        Collections.shuffle(Arrays.asList(mangtenhinh));
         idHinhgoc = getResources().getIdentifier(mangtenhinh[0],"drawable",getPackageName());
         imgHinhgoc.setImageResource(idHinhgoc);
+        imgHinhchon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+            }
+        });
     }
 }
