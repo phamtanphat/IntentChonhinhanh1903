391ea33abb6c518fe74d20d6c0d59c6ca51976eb
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-14 20:23:11 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentchonhinhanh/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentchonhinhanh/MainActivity.java
@@ -2,12 +2,26 @@ package khoapham.ptp.phamtanphat.intentchonhinhanh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgHinhgoc,imgHinhchon;
+    String [] mangtenhinh;
+    int idHinhgoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgHinhchon = findViewById(R.id.imageviewHinhchon);
+        imgHinhgoc = findViewById(R.id.imageviewHinhgoc);
+
+        mangtenhinh = getResources().getStringArray(R.array.arrayanimal);
+        idHinhgoc = getResources().getIdentifier(mangtenhinh[0],"drawable",getPackageName());
+        imgHinhgoc.setImageResource(idHinhgoc);
+
+
     }
 }
